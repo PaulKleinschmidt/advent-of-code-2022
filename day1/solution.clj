6f36ce5f6d2e9e7c830fc3b4cd217f95a21fae5d
Paul Kleinschmidt <paul.kleinschmidt1@gmail.com>
2022-12-07 18:21:17 -0800

    Day 1 Part 2

--- day1/solution.clj
+++ day1/solution.clj
@@ -4,13 +4,25 @@
 (defn parse-int [s]
   (if (number? s) s (Integer/parseInt s)))
 
-(defn highest-calories []
+(defn part-1 []
   (let [input (as-> (slurp "day1/input.txt") $
                 (str/split $ #"\n\n")
                 (map #(str/split % #"\n") $))]
     (apply max (map #(reduce (fn [total current] (+ (parse-int total) (parse-int current))) 0 %) input))))
 
+(defn part-2 []
+  (let [input (as-> (slurp "day1/input.txt") $
+                (str/split $ #"\n\n")
+                (map #(str/split % #"\n") $))
+        totals (map #(reduce (fn [total current] (+ (parse-int total) (parse-int current))) 0 %) input)]
+    (->> totals
+         sort
+         reverse
+         (take 3)
+         (apply +))))
+
 
 (comment 
-  (highest-calories) ;;66487
+  (part-1) ;; 66487
+  (part-2) ;; 197301
   )
