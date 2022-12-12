ee634c11fdcf420ce97349eddb252e177d180bfa
Paul Kleinschmidt <paul.kleinschmidt1@gmail.com>
2022-12-12 15:24:22 -0800

    Day 4 part 1

---
+++ day4/solution.clj
@@ -0,0 +1,30 @@
+(ns day4.solution
+  (:require [clojure.string :as str]
+            [utils :refer [parse-file]]))
+
+(def input (parse-file "day4/input.txt"))
+
+(def get-range (fn [arr]
+                 (vec (range (Integer/parseInt (get arr 0))
+                             (+ 1 (Integer/parseInt (get arr 1)))))))
+
+;; Accepts two arrays, returns true if one of the arrays fully contains the other
+(defn fully-contians? [sections]
+  (let [x (first sections)
+        y (last sections)]
+    (or (every? (set x) y)
+        (every? (set y) x))))
+
+(defn part-1
+  []
+  (reduce (fn [total current]
+         (->> (str/split current #",")
+              (map #(str/split % #"-"))
+              (map get-range)
+              (fully-contians?)
+              (get {false 0 true 1})
+              (+ total))) 0 input))
+
+(comment 
+  (part-1) ;; 511
+  )
